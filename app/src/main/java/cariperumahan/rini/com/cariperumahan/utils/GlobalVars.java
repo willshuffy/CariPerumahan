de93f0ef4f6aa618764b2720d75c603c60b4cf6a
root <root@localhost.localdomain>
2019-01-06 21:36:08 +0700

    bug fix - show data rumah api

--- app/src/main/java/cariperumahan/rini/com/cariperumahan/utils/GlobalVars.java
+++ app/src/main/java/cariperumahan/rini/com/cariperumahan/utils/GlobalVars.java
@@ -2,6 +2,8 @@ package cariperumahan.rini.com.cariperumahan.utils;
 
 public class GlobalVars {
     //public static final String BASE_IP = "192.168.100.31/api_rumah/";
-    public static final String BASE_IP = "192.168.43.174/api_rumah/";
+//    public static final String BASE_IP = "192.168.43.174/api_rumah/";
+    public static final String BASE_IP = "http://willproject.000webhostapp.com/api_rumah/";
+
 
 }
